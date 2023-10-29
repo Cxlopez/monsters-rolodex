c71da8d29d8f3c06ecc8d516f9a0c5c4ee5cb52b
Cxlopez <cristian4402@hotmail.com>
2023-10-29 15:37:55 -0400

    set state to chnage name

--- src/App.js
+++ src/App.js
@@ -19,11 +19,19 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.copmany}
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
           </p>
-            <button onClick={() => {
-              this.setState({ name: "Estuardo" });
-            }}>
+            <button 
+            onClick={() => {
+              this.setState((state, props) => {
+                return {
+                  name: {firstName: 'Estuardo', lastName: 'Sanchez-Lopez'},
+                }
+              }, () => {
+                console.log(this.state);
+              });
+            }}
+            >
               Change Name
             </button>
         </header>
