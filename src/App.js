9fe1988b62a1ce02453e6b21ef5176e73647576d
Cxlopez <cristian4402@hotmail.com>
2023-10-30 19:46:15 -0400

    add key an dkey id to monsters

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
-import { Component } from 'react';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-
   constructor() {
     super();
 
     this.state = {
       monsters: [
-      
-      {
-        name: 'Linda'
-      },
-      {
-        name: 'Frank'
-      },
-      {
-        name: 'Jacky'
-      },
-    ]
-
-    }
-  }  
+        {
+          name: "Linda",
+          id: "2k356kjnk2j",
+        },
+        {
+          name: "Frank",
+          id: "2k3jnb45k2j",
+        },
+        {
+          name: "Jacky",
+          id: "ds987f6nb45k2j",
+        },
+        {
+          name: "Cristian",
+          id: "4h65ht45k2jdg",
+        },
+      ],
+    };
+  }
 
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => {
-            return <h1>{monster.name}</h1>
-          })
-        }
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
-
-  
 }
 
 export default App;
