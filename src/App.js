aa8b864a9515dcd88340f1467fe8b3ef8c098178
Cxlopez <cristian4402@hotmail.com>
2023-11-03 21:49:23 -0400

    remove hard code monster

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
-          );
+          ); 
         })}
       </div>
     );
