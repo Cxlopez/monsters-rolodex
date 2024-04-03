01e6f07290f57c0b07f9d5f0376a161bcd3e9f0e
Cxlopez <cristian4402@hotmail.com>
2024-04-03 15:43:02 -0400

    initialize search state

--- src/App.js
+++ src/App.js
@@ -38,8 +38,11 @@ class App extends Component {
 
   render() {
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
      });
 
     console.log("render");
@@ -49,7 +52,7 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="search monsters"
-          onChange={this.onSearchChange}
+          onChange={onSearchChange}
         />
         {filteredMonsters.map((monster) => {
           return (
