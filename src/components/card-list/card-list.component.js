93aa09d18b812d2a8eda6004d460ab568e703e05
Cxlopez <cristian4402@hotmail.com>
2024-04-03 20:11:46 -0400

    create card-list componnent

---
+++ src/components/card-list/card-list.component.js
@@ -0,0 +1,14 @@
+import { component } from 'react';
+
+class CardList extends component {
+
+  render() {
+    return (
+      <div>
+        Hello im the cardlist component
+      </div>
+    )
+  }
+}
+
+export default CardList;
