94800df7369d9e2d757cd8d6b4adef1cb75fbe2f
Cxlopez <cristian4402@hotmail.com>
2024-04-08 14:17:07 -0400

    retrieve monster email and add id

--- src/components/card-list/card-list.component.js
+++ src/components/card-list/card-list.component.js
@@ -8,8 +8,10 @@ class CardList extends Component {
     return (
       <div>
         {monsters.map(monster => (
-         <div className='card-container'>
+         <div className='card-container' key={monster.id}>
           <img src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} alt={`monster ${monster.name}`} />
+          <h2>{monster.name}</h2>
+          <p>{monster.email}</p>
          </div>
         ))}
       </div>
