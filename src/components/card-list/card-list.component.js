2f38aa9ca5126259a27c884bd2952c61e8347365
Cxlopez <cristian4402@hotmail.com>
2024-04-08 14:21:18 -0400

    desctructure cardList

--- src/components/card-list/card-list.component.js
+++ src/components/card-list/card-list.component.js
@@ -7,13 +7,16 @@ class CardList extends Component {
     
     return (
       <div>
-        {monsters.map(monster => (
-         <div className='card-container' key={monster.id}>
-          <img src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} alt={`monster ${monster.name}`} />
-          <h2>{monster.name}</h2>
-          <p>{monster.email}</p>
+        {monsters.map((monster) => {
+          const { name, email, id } = monster;
+
+          return (
+         <div className='card-container' key={id}>
+          <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${name}`} />
+          <h2>{name}</h2>
+          <p>{email}</p>
          </div>
-        ))}
+        )})}
       </div>
     )
   }
