88a51490b29ba79a0aa67d7579226dcb1f917311
Cxlopez <cristian4402@hotmail.com>
2024-04-09 17:25:42 -0400

    create heading & style

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ class App extends Component {
     console.log("render");
     return (
       <div className="App">
+        <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBox 
         className='search-box'
         onChangeHandler={onSearchChange} 
